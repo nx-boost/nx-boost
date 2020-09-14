2314547eb82f9143012ba5064f11f16e287b77a1
Philipp Seitz <44158030+PhilippSeitz@users.noreply.github.com>
2020-09-14 17:25:21 +0200

    write files and collect statistics

--- packages/purge-css/src/builders/build/builder.ts
+++ packages/purge-css/src/builders/build/builder.ts
@@ -3,11 +3,16 @@ import {
   BuilderOutput,
   createBuilder,
 } from '@angular-devkit/architect';
-import { from, Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { forkJoin, from, Observable, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { BuildBuilderSchema, UserDefinedOptions } from './schema';
 import { PurgeCSS } from 'purgecss';
 import { Path, join } from '@angular-devkit/core';
+import { promisify } from 'util';
+import * as fs from 'fs';
+
+const writeFileAsync = promisify(fs.writeFile);
+const statAsync = promisify(fs.stat);
 
 function getOptions(
   schema: BuildBuilderSchema,
@@ -24,6 +29,15 @@ function getOptions(
   };
 }
 
+async function writeFile(result: { css: string; file?: string }) {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const file = result.file!;
+  const statsBefore = await statAsync(file);
+  await writeFileAsync(file, result.css);
+  const statsAfter = await statAsync(file);
+  return { ...result, stats: { statsBefore, statsAfter } };
+}
+
 export function runBuilder(
   schema: BuildBuilderSchema,
   context: BuilderContext
@@ -33,6 +47,9 @@ export function runBuilder(
       getOptions(schema, join(context.workspaceRoot as Path, schema.outputPath))
     )
   ).pipe(
+    switchMap((results) =>
+      forkJoin(results.map((result) => from(writeFile(result))))
+    ),
     map((_) => ({ success: true })),
     catchError((err) => of({ success: false, error: err.toString() }))
   );
